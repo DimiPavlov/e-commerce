4bac1fc48bfabf138245873b99208c2c7cae0f7f
Dimitar Pavlov <dpavlov@Dimitars-MacBook-Pro.local>
2021-07-29 22:15:44 +0300

    integrating sign up with email and password

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 import { Route, Switch } from "react-router-dom"
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 import HomePage from "./pages/homepage/Homepage";
 import ShopPage from "./pages/shop/Shoppage"
@@ -23,14 +23,25 @@ class App extends Component {
 	unsubscribeFromAuth = null
 
 	componentDidMount() {
-		this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-			this.setState({ 
-				currentUser: user
-			 })
-			 console.log(user)
-		})
-		
-	}
+		this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+		  if (userAuth) {
+			const userRef = await createUserProfileDocument(userAuth);
+	
+			userRef.onSnapshot(snapShot => {
+			  this.setState({
+				currentUser: {
+				  id: snapShot.id,
+				  ...snapShot.data()
+				}
+			  });
+	
+			  console.log(this.state);
+			});
+		  }
+	
+		  this.setState({ currentUser: userAuth });
+		});
+	  }
 
 
 	componentWillUnmount() {
